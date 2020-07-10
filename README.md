8a86a6f3e53cc2ee5791f1374169a8bec5d4ef15
pangyuteng <pangyuteng@gmail.com>
2020-07-09 22:56:47 -0700

    working version

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # papaya-flask-celery
+
+
+A hello world sample that demonstrate how to show nifti images by leveraging papaya, flask and celery.
+
+The dead simpler alternative when we are considering to scale the app is to cut off dependency on Celery, and slap a load balancer in front of Flask, that probably make much more sense.... That said, I'm sticking with celery+rabbitmq+redis, just because they have cuter names than "load balancer".
+
+Papaya - https://github.com/rii-mango/Papaya
+
+Flask - https://github.com/pallets/flask
+
+Celery - https://github.com/celery/celery
+
+Docker - https://www.docker.com
+
+## build with docker, then start Flask and underlying services.
+
+```
+docker-compose build
+docker-compose up
+```
+
+## check out the images with the Papaya viewer that you are now hosting!
+
++ head over to localhost:5000 and click on one of the mocked links.
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import time
 import random
 import string
 import tempfile
+import base64
 
 import numpy as np
 import SimpleITK as sitk
@@ -15,26 +16,34 @@ from flask import (
     session, flash, redirect, url_for, jsonify
 )
 
+
 app = Flask(__name__)
-app.config["CELERY_BROKER_URL"] = os.environ["AMQP_URI"]
-app.config["CELERY_RESULT_BACKEND"] = os.environ["REDIS_URI"]
 app.config["SECRET_KEY"] = "the random string"
 
-celery = Celery(app.name, broker=app.config["CELERY_BROKER_URL"])
-celery.conf.update(app.config)
+celery_config = {
+    "broker_url": os.environ["AMQP_URI"],
+    "result_backend": os.environ["REDIS_URI"],
+    "task_serializer": "pickle", # for passing binary objects
+    "result_serializer": "pickle",
+    "accept_content": ["pickle"],
+}
+
+celery = Celery(app.name, broker=celery_config["broker_url"])
+celery.conf.update(celery_config)
 
 @celery.task(bind=True)
 def _get_image(self,uid):
-    #time.sleep(10)
-    #return f"THIS IS THE RESULT {uid}"
-
     with tempfile.TemporaryDirectory() as tempdir:
+        use_compression = False
+        fpath = os.path.join(tempdir,'img.nii')
+        
+        # passing binary object is a bad idea since it'll blow up redis
+        # check refs 19231389, 14118526 for alternatives/inspirations
+
         spacing = (0.6,0.6,1.8)
         origin = (0,0,1)
         direction = (0,1,0,1,0,0,0,0,1)
-        use_compression = False
-        fpath = os.path.join(tempdir,'img.nii')
-        arr = (np.random.rand(256,256,10)*255).astype(np.uint8)
+        arr = (np.random.rand(128,128,50)*255).astype(np.uint8)
         img = sitk.GetImageFromArray(arr)
         img.SetSpacing(spacing)
         img.SetOrigin(origin)
@@ -45,13 +54,16 @@ def _get_image(self,uid):
         writer.Execute(img)
 
         with open(fpath,'rb') as f:
-            return f.read()
+            binary_file_data = f.read()
+            base64_encoded_data = base64.b64encode(binary_file_data)
+            base64_message = base64_encoded_data.decode('utf-8')
+            return {"ext":".nii","base64string":base64_message}
 
 @app.route('/', methods=['GET'])
 def index():
     mylist = []
     for _ in range(10):
-        mystatus = ' '+''.join(random.choice(string.ascii_lowercase) for i in range(10))
+        mystatus = ' '+''.join(random.choice(string.digits+'.') for i in range(64))
         mylist.append(mystatus)
     return render_template('index.html',mylist=mylist)
 
@@ -85,13 +97,16 @@ def get_image(series_instance_uid):
 def show_image():
     myinfo = session['myinfo']       # counterpart for session
     myinfo = json.loads(myinfo)
-    print(myinfo)
     series_instance_uid = myinfo["series_instance_uid"]
     task_id = myinfo["task_id"]
     task = celery.AsyncResult(task_id)
     print(series_instance_uid,task_id,task.ready())
     if task.ready() is True:
-        return render_template("show_image.html", series_instance_uid=series_instance_uid,task_id=task_id,mydata=task.get())
+        return render_template("show_image.html", 
+            series_instance_uid=series_instance_uid,
+            task_id=task_id,
+            ext=task.get()["ext"],
+            base64string=task.get()["base64string"],)
     else:
         time.sleep(1)
         status = ' '+''.join(random.choice(string.ascii_lowercase) for i in range(10))
@@ -105,6 +120,20 @@ if __name__ == '__main__':
     app.run(debug=True,host="0.0.0.0",port=args.port)
 
 """
+ref. 
+flask+celery:
 https://stackoverflow.com/questions/26954122/how-can-i-pass-arguments-into-redirecturl-for-of-flask/26957478
 https://stackoverflow.com/questions/17057191/redirect-while-passing-arguments
+https://stackoverflow.com/questions/41956480/how-to-pass-large-chunk-of-data-to-celery/46072582#46072582
+https://stackoverflow.com/questions/31866796/making-an-asynchronous-task-in-flask
+passing binary object via celery:
+https://stackoverflow.com/questions/14118526/how-to-serialize-binary-files-to-use-with-a-celery-task
+https://stackoverflow.com/questions/19231389/celery-task-in-flask-for-uploading-and-resizing-images-and-storing-it-to-amazon
+# papaya + display images:
+https://github.com/vsoch/nifti-drop
+https://github.com/rii-mango/Papaya/issues/32
+https://github.com/rii-mango/Papaya/pull/79
+# convert byte obect to base 64 encoded string
+https://github.com/rii-mango/Papaya/wiki/Configuration#images
+https://stackabuse.com/encoding-and-decoding-base64-strings-in-python/
 """
