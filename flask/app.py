968d5ec55acf6ff0a15aa12b7341b7447df220be
pangyuteng <pangyuteng@gmail.com>
2020-07-25 04:23:31 -0700

    added flask-login exmpale

--- flask/app.py
+++ flask/app.py
@@ -13,6 +13,8 @@ from flask import (
     session, flash, redirect, url_for, jsonify,
     send_file
 )
+import flask
+import flask_login
 
 import utils
 
@@ -47,6 +49,75 @@ def gen_random(num):
     for _ in range(10):
         mylist.append(' '+''.join(random.choice(string.ascii_letters) for i in range(32)))
     return mylist
+
+
+# https://github.com/maxcountryman/flask-login
+login_manager = flask_login.LoginManager()
+login_manager.init_app(app)
+
+class User(flask_login.UserMixin):
+    pass
+
+users = {
+    'test@gmail.com':{'password':'password'},
+}
+@login_manager.user_loader
+def user_loader(email):
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+    return user
+
+@login_manager.request_loader
+def request_loader(request):
+    email = request.form.get('email')
+    if email not in users:
+        return
+
+    user = User()
+    user.id = email
+
+    # DO NOT ever store passwords in plaintext and always compare password
+    # hashes using constant-time comparison!
+    user.is_authenticated = request.form['password'] == users[email]['password']
+
+    return user
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if flask.request.method == 'GET':
+        return '''
+               <form action='login' method='POST'>
+                <input type='text' name='email' id='email' placeholder='email'/>
+                <input type='password' name='password' id='password' placeholder='password'/>
+                <input type='submit' name='submit'/>
+               </form>
+               '''
+
+    email = flask.request.form['email']
+    if flask.request.form['password'] == users[email]['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+
+    return 'Bad login'
+
+@app.route('/logout')
+def logout():
+    flask_login.logout_user()
+    return 'Logged out'
+
+@login_manager.unauthorized_handler
+def unauthorized_handler():
+    return 'Unauthorized'
+
+@app.route('/protected')
+@flask_login.login_required
+def protected():
+    return 'Logged in as: ' + flask_login.current_user.id
 
 @app.route('/', methods=['GET'])
 def index():
