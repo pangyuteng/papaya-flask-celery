f7710a1f66bf13d87583b63e0d0249fe90c8ba15
pangyuteng <pangyuteng@gmail.com>
2021-03-14 19:24:28 -0700

    fixed redirect after upload

--- dragndrop/README.md
+++ dragndrop/README.md
@@ -9,7 +9,14 @@ docker-compose up
 ```
 + head to `localhost:5000`, and drag in multiple `.csv` files.
 
+### dev
 
++ development mode
+```
+docker-compose build
+docker run -it -p 5000:5000 -v $PWD:/opt/code dragndrop_dropzone bash
+python app
+```
 ### ref
 + https://github.com/dropzone/dropzone/releases/download/v5.8.1/dist.zip
 + https://stackoverflow.com/a/42264730/868736
--- dragndrop/app.py
+++ dragndrop/app.py
@@ -1,4 +1,6 @@
 import os
+import traceback
+import shutil
 import tempfile
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
@@ -20,25 +22,33 @@ def upload():
     if request.method == 'POST':
         csv_list = []
         myfile = request.files.getlist('file')
-        if not isinstance(myfile,list):
-            myfile = [myfile]
         for f in myfile:
             file_path = os.path.join(tempdir, f.filename)
             f.save(file_path)
-    return redirect(url_for('completed',uid=uid))
+    return "ok"
 
 @app.route('/completed')
 def completed():
     uid = request.args.get('uid')
     tempdir = os.path.join('/tmp',uid)
     df_list = []
-    for basename in os.listdir(tempdir):
-        file_path = os.path.join(tempdir,basename)
-        if not file_path.endswith('.csv'):
-            continue
-        df = pd.read_csv(file_path)
-        df_list.append(df)
-    return render_template('completed.html',uid=uid,df_list=df_list)
+
+    if os.path.exists(tempdir):
+        for basename in os.listdir(tempdir):
+            file_path = os.path.join(tempdir,basename)
+            if not file_path.endswith('.csv'):
+                continue
+            df = pd.read_csv(file_path)
+            df_list.append(df)
+
+    try:
+        shutil.rmtree(tempdir)
+        delete_success = True
+    except:
+        traceback.print_exc()
+        delete_success = False
+        
+    return render_template('completed.html',uid=uid,df_list=df_list,delete_success=delete_success)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=5000, debug=True)
